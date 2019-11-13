6b520e822a3e558ec4501f96b390e4ee29350e20
Phong Yang <phongyang88@hotmail.com>
2019-11-13 16:38:16 -0600

    finished all challenges and the stretch goal

--- index.js
+++ index.js
@@ -39,9 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name} and ${this.age}`;
+}
+
+
+
+
 
 /*
   TASK 2
@@ -57,8 +77,32 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.milesPerGallon = milesPerGallon
+  this.model = model;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance){
+  let maxDistance = this.tank * this.milesPerGallon;
+
+  if (distance > maxDistance){
+    distance = maxDistance;
+  }
+
+  this.odometer += distance;
+  this.tank -= (distance/this.milesPerGallon);
+
+  
+
+  if (this.tank <= 0){
+    return `I ran out of fuel at ${this.odometer} miles`;
+  }
 }
 
 /*
@@ -68,15 +112,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+  Person.call(this, name, age);
+}
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
+  1. The Window/Global Object binding is when 
   2. 
   3. 
   4. 
